e59a056e07f4591ec8039b1536af4e0d1f189dd5
Sue609 <susankamau609@gmail.com>
2023-11-27 21:11:16 +0000

    JavaScript ES6

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employeeNames = [];
-
-  for (const department in reportWithIterator) {
-    const employees = reportWithIterator[department];
-    for (const employee of employees) {
-      employeeNames.push(employee);
-    }
-  }
-
-  return employeeNames.join(' | ');
+  return reportWithIterator.join(' | ');
 }

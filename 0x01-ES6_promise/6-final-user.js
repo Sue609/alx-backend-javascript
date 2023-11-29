0694e16b21991d9dd70d577b20b54548c87e4180
Sue609 <susankamau609@gmail.com>
2023-11-29 11:48:43 +0000

    modifying the javascript functions to adhere to eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,25 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => {
-      const formattedResults = results.map((result) => {
-        if (result.status === 'fulfilled') {
-          return {
-            status: 'fulfilled',
-            value: result.value,
-          };
-        }
-        return {
-          status: 'rejected',
-          value: result.reason,
-        };
-      });
-      return formattedResults;
-    })
-    .catch((error) => [{ status: 'rejected', value: error }]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }

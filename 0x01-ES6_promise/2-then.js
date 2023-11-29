119aa4b90297113f687d6113f8539bd2f097cf7e
Sue609 <susankamau609@gmail.com>
2023-11-29 11:41:39 +0000

    modifying the javascript functions to adhere to eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,9 +2,17 @@ function handleResponseFromAPI(promise) {
   promise
     .then(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
+      return {
+        status: 200,
+        body: 'success',
+      };
     })
-    .catch(() => new Error(''));
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    })
+    .finally(() => {
+    });
 }
 
 export default handleResponseFromAPI;

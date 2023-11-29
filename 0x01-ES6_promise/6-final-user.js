777def6c12cf6816ffd616ee9db19dfd81b344ac
Sue609 <susankamau609@gmail.com>
2023-11-29 06:01:06 +0000

    Handling multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,25 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+      const formattedResults = results.map((result) => {
+        if (result.status === 'fulfilled') {
+          return {
+            status: 'fulfilled',
+            value: result.value,
+          };
+        }
+        return {
+          status: 'rejected',
+          value: result.reason,
+        };
+      });
+      return formattedResults;
+    })
+    .catch((error) => [{ status: 'rejected', value: error }]);
+}

54e47d766662c77b56762101ca0b4320ef114c91
Sue609 <susankamau609@gmail.com>
2023-11-30 11:30:43 +0000

    Implement getter and setter methods

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
-import ClassRoom from './0-classroom'
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-   const sizes =[19, 20, 34];
-   const rooms = sizes.map(size => new ClassRoom(size));
-   return rooms;
+  const sizes = [19, 20, 34];
+  const rooms = sizes.map((size) => new ClassRoom(size));
+  return rooms;
 }
 export default initializeRooms;

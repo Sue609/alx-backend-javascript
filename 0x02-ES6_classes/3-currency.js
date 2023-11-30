105913d6c2e9cd4f397b64e171543c2ad8dc7667
Sue609 <susankamau609@gmail.com>
2023-11-30 11:52:51 +0000

    implementing a class named currency with getters and setters and a method.

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string' || typeof name !== 'string') {
+      throw new TypeError('Invalid input type');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+export default Currency;

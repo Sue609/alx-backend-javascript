1c18e3115da4861f10a7d550de7b72cd39050c16
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-12-06 15:31:58 +0300

    writing a string literal named Subjects which allows a variable to have the value math or history only

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -55,3 +55,15 @@ export class Director implements DirectorInterface {
     }
     return (employee as Teacher).workTeacherTasks();
   }
+
+  export type Subjects = ('Math' | 'History');
+
+  export function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+      return 'Teaching Math';
+    }
+    if (todayClass === 'History') {
+      return 'Teaching History';
+    }
+  }
+    
